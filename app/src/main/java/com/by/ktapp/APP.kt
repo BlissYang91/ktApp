a2820eff6a07a28ff4ddcae80e8ec9d06f2e04ac
yangtianfu <2812420513@qq.com>
2023-07-27 17:51:48 +0800

    init

---
+++ app/src/main/java/com/by/ktapp/APP.kt
@@ -0,0 +1,15 @@
+package com.by.ktapp
+
+import android.app.Application
+
+/**
+ * @Author yangtianfu
+ * @Date 2023/7/27 15:28
+ * @Describe 
+ */
+class APP: Application() {
+    
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
